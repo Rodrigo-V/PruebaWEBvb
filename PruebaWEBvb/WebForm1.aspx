503913568b1eda70e5418153ec6512df9ad5286c
RODRIGO VILCHEZ FERNANDEZ <rvilchezf@outlook.com>
2019-03-08 13:48:14 -0300

    datagrid conectado a sql por codigo llenado con empleados

---
+++ PruebaWEBvb/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="PruebaWEBvb.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Prueba de VB</title>
+</head>
+<body>
+    <h5>Hola Mundo</h5>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ PruebaWEBvb/WebForm1.aspx.vb
@@ -0,0 +1,35 @@
+﻿
+Imports System.Data.SqlClient
+
+
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim conn As String = "Data Source=LAPTOP-GUIQPSQR\SQLEXPRESS;Initial Catalog=Farmacia;Integrated Security=True"
+        Dim sel As String = "Select * from Empleado"
+
+        Dim da As SqlDataAdapter
+        Dim dt As New DataTable
+
+        Try
+            da = New SqlDataAdapter(sel, conn)
+            da.Fill(dt)
+
+            GridView1.DataSource = dt
+            GridView1.DataBind()
+            Label1.Text = String.Format("Total datos en la tabla: {0}", dt.Rows.Count)
+
+        Catch ex As Exception
+            Label1.Text = "Error: " & ex.Message
+
+        End Try
+
+
+
+
+
+
+    End Sub
+
+End Class
